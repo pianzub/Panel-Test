85d6a4d1f560775b1b87c2c37832f463a72cb8f1
pianzub <pianzub@gmail.com>
2026-08-26 18:57:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // Ganti dengan URL deployment Web App Apps Script yang berakhiran /exec.
-  API_URL: 'https://script.google.com/macros/s/AKfycbzdcfDNQZXT-fnFymcopozXwN4ovhDdFlhaC1sfO1PRIL9mXmFntguuQNQf7lSgqmwD/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbygiP3B1zT4XBYn42wNddOQX3QMMVwQuJ6iQODAAQcYAKHL-vyL5b4gvx6xcQTvnFrSdg/exec'
 };
